e0ccbe8385b52358125ba0c59c05a417e1076712
stino-x <107426247+stino-x@users.noreply.github.com>
2023-08-26 22:24:06 +0200

    adjusted styling to fit requirements

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
+// import './App.css';
 import {
   Route, RouterProvider, createBrowserRouter, createRoutesFromElements,
 } from 'react-router-dom';
 import BookLayout from './components/BookLayout';
 import { ContextProvider } from './ContextProvider';
 import Books from './components/Book-List/Books';
-import Form from './components/Form/Form';
+import Category from './components/Category/Category';
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<BookLayout />}>
         <Route index element={<Books />} />
-        <Route path="form" element={<Form />} />
+        <Route path="category" element={<Category />} />
       </Route>,
     ),
   );
--- src/components/BookLayout.js
+++ src/components/BookLayout.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 
 export default function BookLayout() {
+  const location = useLocation();
+
+  const isHomeRoute = location.pathname === '/';
+  // const isAboutRoute = location.pathname === '/about';
   return (
     <>
-      <nav>
-        <li className="nav-items"><NavLink to="/" className="nav-link">Books</NavLink></li>
-        <li className="nav-items"><NavLink to="form" className="nav-link">Form</NavLink></li>
+      <nav className={isHomeRoute ? 'books-section' : 'nav-category'}>
+        <span className={isHomeRoute ? 'icon' : 'icon-category'}>
+          Bookstore  CMS
+        </span>
+        <ul>
+          <li className="books"><NavLink to="/" className="nav-link books">Books</NavLink></li>
+          <li className="categories"><NavLink to="category" className="nav-link categories">Categories</NavLink></li>
+        </ul>
       </nav>
       <Outlet />
+      <footer />
     </>
   );
 }
